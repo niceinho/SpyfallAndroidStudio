68da9c3c16c5013973f98a66ad32635f3c387910
Joseph Chang <niceinho@gmail.com>
2018-10-30 14:01:20 -0400

    Created array for locations and assigned a random location

--- app/src/main/java/com/example/nicei/spyfall/LocationDisplay.java
+++ app/src/main/java/com/example/nicei/spyfall/LocationDisplay.java
@@ -3,13 +3,17 @@ package com.example.nicei.spyfall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class LocationDisplay extends AppCompatActivity {
+
+    private TextView location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_display);
+        location = findViewById(R.id.locationList);
     }
 
     public void abort(View v) {
